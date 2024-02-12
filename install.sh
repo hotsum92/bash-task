5d323ba1940aa1ad4f9f7b3bc140b97e5fca2c55
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-02-13 01:34:57 +0900

    implement

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cp cd-task /usr/local/bin
+cp completions/cd-task /usr/share/bash-completion/completions
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm /usr/local/bin/cd-task
+rm /usr/share/bash-completion/completions/cd-task
