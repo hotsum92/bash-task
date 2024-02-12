635a2a72fa9c694510acfbd958eca0b9a51b1221
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-02-13 01:38:23 +0900

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+cp mk-task /usr/local/bin
 cp cd-task /usr/local/bin
 cp completions/cd-task /usr/share/bash-completion/completions
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+rm /usr/local/bin/mk-task
 rm /usr/local/bin/cd-task
 rm /usr/share/bash-completion/completions/cd-task
